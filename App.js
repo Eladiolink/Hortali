e7c050cf32519d2bd2a791309713756ac9dd9fbd
Eládio Alves <Eladiolink1@gmail.com>
2024-04-03 09:32:39 -0300

    Inicio da Nav Tabs

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Cadastro from "./src/views/Cadastro/Cadastro";
 import CadastroContinuacao from "./src/views/Cadastro/CadastroContinuacao";
 import TelaContrato from "./src/views/TelaContrato/TelaContrato";
 import Retorno from "./src/views/TelaContrato/Retorno";
+import Principal from "./src/views/Principal/Principal";
 
 
 const Stack = createStackNavigator()
@@ -21,25 +22,35 @@ export default App = () => {
     "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
   });
 
-  
+
   if (!fontsLoaded) return null
+  var login = true
+  if (login) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Principal></Principal>
+      </SafeAreaView>
+    )
+  } else {
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}  >
 
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Cadastro" component={Cadastro} />
-          <Stack.Screen name="CadastroContinuacao" component={CadastroContinuacao} />
-          <Stack.Screen name="RememberPassword" component={EsqueceuSenha} />
-          <Stack.Screen name="TelaContrato" component={TelaContrato} />
-          <Stack.Screen name="Retorno" component={Retorno} />
+    return (
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}  >
 
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-  );
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Cadastro" component={Cadastro} />
+            <Stack.Screen name="CadastroContinuacao" component={CadastroContinuacao} />
+            <Stack.Screen name="RememberPassword" component={EsqueceuSenha} />
+            <Stack.Screen name="TelaContrato" component={TelaContrato} />
+            <Stack.Screen name="Retorno" component={Retorno} />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
